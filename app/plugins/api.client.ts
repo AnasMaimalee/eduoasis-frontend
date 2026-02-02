9e19e751608b373b98366ebc7d8a7e5ef3054433
AnasMaimalee <anasment6@gmail.com>
2026-02-02 14:34:13 +0530

    fix: added new backend url

--- app/plugins/api.client.ts
+++ app/plugins/api.client.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin(() => {
   const authStore = useAuthStore()
 
   const api = $fetch.create({
-    baseURL: 'https://robertson-boot-opportunities-orientation.trycloudflare.com/api',
+    baseURL: 'https://fishing-houston-conference-geneva.trycloudflare.com/api',
 
     onRequest({ options }) {
       if (authStore.token) {
